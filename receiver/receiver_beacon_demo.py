d004f523516f1b955d0633c16dcebe27795fe2d0
Rustam Mehmandarov <mehmandarov@users.noreply.github.com>
2021-11-30 14:52:40 +0100

    Update receiver_beacon_demo.py

--- receiver/receiver_beacon_demo.py
+++ receiver/receiver_beacon_demo.py
@@ -1,6 +1,5 @@
 def on_button_pressed_a():
     radio.send_string("DUCK")
-input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_received_string(receivedString):
     global signal
@@ -13,12 +12,21 @@ def on_received_string(receivedString):
     else:
         # basic.show_string(receivedString)
         led.plot_bar_graph(Math.map(signal, -128, -42, 0, 9), 9)
+
+def led_init():
+    global LED
+    LED = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+    LED.clear()
+    LED.range(0, 3).show_color(neopixel.colors(NeoPixelColors.ORANGE))
+    LED.set_brightness(10)
+
+
+# Fire events:
+input.on_button_pressed(Button.A, on_button_pressed_a)
 radio.on_received_string(on_received_string)
 
 signal = 0
 LED: neopixel.Strip = None
-LED = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
-LED.clear()
-LED.range(0, 3).show_color(neopixel.colors(NeoPixelColors.ORANGE))
-LED.set_brightness(10)
-radio.set_group(1)
+
+led_init()
+radio.set_group(5)
